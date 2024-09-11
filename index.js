54b0e7b737d0e9f8aebe816dbc71763795aa33f3
arthurdev01 <arthur.spinosa12@gmail.com>
2024-09-11 15:14:33 -0300

    fundamentos004

--- index.js
+++ index.js
@@ -1,37 +1,8 @@
-// hello world 
- //const mensagem = "ola eu"
-
-
-// {
-  //  const mensagem = "ola, mundo" 
- //}
-
-//console. log(mensagem); 
-
-// arrays, objetos 
-
-//let metas = ["mayk", "alo"]
-
-//console.log(metas[1])
-
-//let meta = { 
-   //value: 'ler um livro por mes',
-   //checked: false 
-
-//}
-
-//console.log(meta.value)
-
-//function //arrow function 
-
-//const criarMeta = () =>{}
-
-   let metas = [ 
-         metas,
-         {
-               value: "caminhar 20 min",
-               checked: false
-
-         }
-   ]
-console.log (metas[1], value)
+const start = () => {
+      let count = 0
+      while(count <10 ){
+         console.log(count)
+         count = count +1
+      }
+}
+start ()
